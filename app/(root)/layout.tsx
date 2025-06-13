0111b631e610189917e93e0f85e8340e02000fdd
Abdurrehman <education24to7@gmail.com>
2025-06-13 23:30:21 +0500

    UI changes

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -16,7 +16,7 @@ const Layout = async ({ children }: { children: ReactNode }) => {
       <nav className="flex">
         <Link href="/" className="flex items-center gap-2">
           <Image src="/logo.png" alt="MockMate Logo" width={38} height={32} />
-          <h2 className="text-primary-100">Jogify.ai</h2>
+          <h2 className="text-primary-100">Jobify.ai</h2>
         </Link>
 
         <Button className="btn-primary ml-auto" onClick={signOut}>
